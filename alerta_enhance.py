5ce18d6a887c6e2f1a103352f50f529a6645e8fb
Александр Лисовенко <alisovenko@sberhealth.ru>
2020-09-25 21:15:47 +0700

    fix bugs, add inhibits

---
+++ alerta_enhance.py
@@ -0,0 +1,34 @@
+import logging
+import os
+
+from alerta.plugins import PluginBase
+
+LOG = logging.getLogger('alerta.plugins.enhance')
+
+RUNBOOK_URL = 'https://wiki.docdoc.pro/wiki/RunBook'
+
+
+class EnhanceAlert(PluginBase):
+
+    def pre_receive(self, alert):
+
+        LOG.info("Enhancing alert...")
+
+        # Set "isOutOfHours" flag for later use by notification plugins
+        dayOfWeek = alert.create_time.strftime('%a')
+        hourOfDay = alert.create_time.hour
+        if dayOfWeek in ['Sat', 'Sun'] or 8 > hourOfDay > 18:
+            alert.attributes['isOutOfHours'] = True
+        else:
+            alert.attributes['isOutOfHours'] = False
+
+        # Add link to Run Book based on event name
+        alert.attributes['runBookUrl'] = '%s/%s' % (RUNBOOK_URL, alert.event.replace(' ', '-'))
+
+        return alert
+
+    def post_receive(self, alert):
+        return
+
+    def status_change(self, alert, status, text):
+        return
